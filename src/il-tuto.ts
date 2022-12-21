a5c5a3336b375f6b4e0c1c87af9d1389df0f6c09
Hugo Mellouet <hugo.mellouet@gmail.com>
2022-12-21 14:58:41 +0100

    colorer différents languages

--- src/il-tuto.ts
+++ src/il-tuto.ts
@@ -1,40 +1,38 @@
 class IlTuto extends HTMLElement {
 
   connectedCallback() {
-    const inputForm = document.getElementById('start') as HTMLFormElement;
+    const start = document.getElementById('start') as HTMLFormElement;
     const pre = document.createElement('pre');
     pre.className = "coder";
-    inputForm.appendChild(pre);
+    start.appendChild(pre);
     const items = document.getElementsByClassName('coder')
-    const display = items[items.length-1]
-    const input = this.getAttribute('text');
-    var text = "";
-    if(input) {
-      text = input.toString();
+    const inputCode = this.getAttribute('code');
+    var codeToDisplay = "";
+    if(inputCode) {
+      codeToDisplay = inputCode.toString();
+    }
+    const inputLanguage = this.getAttribute('language');
+    var languageToColor = "";
+    if(inputLanguage) {
+      languageToColor = inputLanguage.toString();
     }
     var code:any=document.createElement("code");
     code.setAttribute(
-      'class','language-java'
+      'class','language-'+languageToColor
     );
     code.setAttribute(
-      'id','tip-java'
+      'id','tip-'+languageToColor
     );
-    code.appendChild(document.createTextNode(text));
-    display?.appendChild(code);
-        
-    var script1 = document.createElement('script');
-    script1.setAttribute(
-      'id','test1'
-    );
-    script1.innerText="hljs.highlightAll();";
+    code.appendChild(document.createTextNode(codeToDisplay));
+    pre.appendChild(code);
         
     var script2 =document.createElement('script');
     script2.setAttribute(
       'id','test2'
     );
-    script2.innerText="tippy('#tip-java', {content: 'This is java Code',});"
-    inputForm.appendChild(script1);
-    inputForm.appendChild(script2);
+    var language = languageToColor;
+    script2.innerText="tippy('#tip-"+languageToColor+"', {content: 'This is "+language+" Code' });"
+    pre.appendChild(script2);
   }  
 }
 
