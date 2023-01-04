1547e1ce0b6e364b5e0261d94be9ec0711759ace
Hugo Mellouet <hugo.mellouet@gmail.com>
2023-01-04 16:50:40 +0100

    new il-tuto with local files content

--- dist/il-tuto.js
+++ dist/il-tuto.js
@@ -1,30 +1,45 @@
 "use strict";
 class IlTuto extends HTMLElement {
     connectedCallback() {
+        const inputFilePath = this.getAttribute('filePath');
+        var pathFileToDisplay = "";
+        if (inputFilePath) {
+            pathFileToDisplay = inputFilePath.toString();
+        }
+        var languageToColor = "";
+        const inputLanguage = this.getAttribute('language');
+        if (inputLanguage) {
+            languageToColor = inputLanguage.toString();
+        }
+        this.extractContentFromHTMLFile(pathFileToDisplay, languageToColor, this);
+    }
+    extractContentFromHTMLFile(filePath, languageToColor, th) {
+        let fileContents;
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            fileContents = xhr.responseText.toString();
+            th.createHljsContent(fileContents, languageToColor);
+        };
+        xhr.open('GET', filePath);
+        xhr.send();
+    }
+    createHljsContent(content, languageToColor) {
         const start = document.getElementById('start');
         const pre = document.createElement('pre');
         pre.className = "coder";
         start.appendChild(pre);
-        const items = document.getElementsByClassName('coder');
-        const inputCode = this.getAttribute('code');
-        var codeToDisplay = "";
-        if (inputCode) {
-            codeToDisplay = inputCode.toString();
-        }
-        const inputLanguage = this.getAttribute('language');
-        var languageToColor = "";
-        if (inputLanguage) {
-            languageToColor = inputLanguage.toString();
-        }
         var code = document.createElement("code");
         code.setAttribute('class', 'language-' + languageToColor);
         code.setAttribute('id', 'tip-' + languageToColor);
-        code.appendChild(document.createTextNode(codeToDisplay));
+        code.appendChild(document.createTextNode(content));
         pre.appendChild(code);
+        var script1 = document.createElement('script');
+        script1.setAttribute('id', 'test1');
+        script1.innerText = "hljs.highlightAll();";
         var script2 = document.createElement('script');
         script2.setAttribute('id', 'test2');
-        var language = languageToColor;
-        script2.innerText = "tippy('#tip-" + languageToColor + "', {content: 'This is " + language + " Code' });";
+        script2.innerText = "tippy('#tip-" + languageToColor + "', {content: 'This is " + languageToColor + " Code' });";
+        pre.appendChild(script1);
         pre.appendChild(script2);
     }
 }
--- src/il-tuto.ts
+++ src/il-tuto.ts
@@ -1,39 +1,57 @@
 class IlTuto extends HTMLElement {
 
   connectedCallback() {
-    const start = document.getElementById('start') as HTMLFormElement;
-    const pre = document.createElement('pre');
-    pre.className = "coder";
-    start.appendChild(pre);
-    const items = document.getElementsByClassName('coder')
-    const inputCode = this.getAttribute('code');
-    var codeToDisplay = "";
-    if(inputCode) {
-      codeToDisplay = inputCode.toString();
+    const inputFilePath = this.getAttribute('filePath');
+    var pathFileToDisplay = "";
+    if(inputFilePath) {
+      pathFileToDisplay = inputFilePath.toString();
     }
-    const inputLanguage = this.getAttribute('language');
     var languageToColor = "";
+    const inputLanguage = this.getAttribute('language');
     if(inputLanguage) {
       languageToColor = inputLanguage.toString();
     }
-    var code:any=document.createElement("code");
-    code.setAttribute(
-      'class','language-'+languageToColor
-    );
-    code.setAttribute(
-      'id','tip-'+languageToColor
-    );
-    code.appendChild(document.createTextNode(codeToDisplay));
-    pre.appendChild(code);
-        
-    var script2 =document.createElement('script');
-    script2.setAttribute(
-      'id','test2'
-    );
-    var language = languageToColor;
-    script2.innerText="tippy('#tip-"+languageToColor+"', {content: 'This is "+language+" Code' });"
-    pre.appendChild(script2);
-  }  
+    this.extractContentFromHTMLFile(pathFileToDisplay, languageToColor, this)
+  }
+  
+  extractContentFromHTMLFile(filePath: string, languageToColor: string, th: any) {
+    let fileContents;
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      fileContents = xhr.responseText.toString();
+      th.createHljsContent(fileContents, languageToColor);
+      };
+    xhr.open('GET', filePath);
+    xhr.send();
+  }
+
+  createHljsContent(content: string, languageToColor: string): void {
+    const start = document.getElementById('start') as HTMLFormElement;
+      const pre = document.createElement('pre');
+      pre.className = "coder";
+      start.appendChild(pre);
+      var code:any=document.createElement("code");
+      code.setAttribute(
+        'class','language-'+languageToColor
+      );
+      code.setAttribute(
+        'id','tip-'+languageToColor
+      );
+      code.appendChild(document.createTextNode(content));
+      pre.appendChild(code);
+      var script1 = document.createElement('script');
+      script1.setAttribute(
+        'id','test1'
+      );
+      script1.innerText="hljs.highlightAll();";
+      var script2 =document.createElement('script');
+      script2.setAttribute(
+        'id','test2'
+      );
+      script2.innerText="tippy('#tip-"+languageToColor+"', {content: 'This is "+languageToColor+" Code' });"
+      pre.appendChild(script1);
+      pre.appendChild(script2);
+  }
 }
 
 customElements.define('il-tuto', IlTuto);
